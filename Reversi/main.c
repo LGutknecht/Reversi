7adc1aac8c70bc07e8a5086101c51620caab0640
LGutknecht <lukasgutknecht@web.de>
2019-03-26 16:01:40 +0100

    Added Function ClearBoard()

--- Reversi/main.c
+++ Reversi/main.c
@@ -2,11 +2,18 @@
 #include <stdlib.h>
 
 /**
+*   Defining placeholders for the color codes
+**/
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+
+/**
 *   Initialising Function Prototypes
 **/
-void DrawBoard(char GameState[8][8]);
+void DrawBoard();
 void DrawHeader();
 void DrawIndexTop();
+void ClearBoard();
 
 /**
 *   Initialising Global Variables
@@ -15,6 +22,8 @@ char GameState[8][8];
 char IndexSide[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'};
 int main()
 {
+    //Clearing the Game Board
+    ClearBoard();
     //Drawing the Game Board
     DrawBoard(GameState);
 }
@@ -24,7 +33,10 @@ int main()
 *   It gets passed the GameState as a char Array of 8x8
 *   The function also displays the current time and the points of each player
 **/
-void DrawBoard(char GameState[8][8]){
+void DrawBoard(){
+    //Drawing the Game Board Header
+    DrawHeader();
+
     //Drawing the Top Index
     DrawIndexTop();
 
@@ -36,6 +48,11 @@ void DrawBoard(char GameState[8][8]){
         printf("\n");
     }
 }
+/**
+*   This Functions Draws the Game Board Header, including Play Time and the Score Board
+**/
+void DrawHeader(){
+}
 
 /**
 *   This Function Draws the Top Index Line for the Navigation on the Game Board
@@ -46,4 +63,16 @@ void DrawIndexTop(){
         printf("%3i", i);
     }
     printf("\n");
+}
+
+/**
+*   This Function Clears the Game Board and resets it to its original state
+**/
+void ClearBoard(){
+    //Clearing the Board
+    for(int i = 0; i < 8; i++){
+        for(int k = 0; k < 8; k++){
+            GameState[i][k] = 'X';
+        }
+    }
 }
