508ed228fc3d6cb2bfb7d074bb74fae851575cf1
LGutknecht <lukasgutknecht@web.de>
2019-04-09 22:51:04 +0200

    Created File Structure

---
+++ Reversi/Reversi.h
@@ -0,0 +1,11 @@
+#ifndef REVERSI_H_INCLUDED
+#define REVERSI_H_INCLUDED
+#include <time.h>
+struct SaveFile{
+    int GameField[8][8];
+    int Turn;
+    int Time;
+};
+
+
+#endif // REVERSI_H_INCLUDED
---
+++ Reversi/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Reversi.h"
+
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
